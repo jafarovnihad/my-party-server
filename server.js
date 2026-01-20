2708315a6e20dd15bfe29abb4a9849ff89ebf142
jafarovnihad <nihad.jafarov06@gmail.com>
2026-01-20 16:35:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,12 +12,9 @@ const io = new Server(server, {
 });
 
 // --- Room Data Storage ---
-const roomUsers = {}; 
-
 function updateRoomStats(room) {
     if (!room) return;
     
-    // Get all unique usernames currently in the room
     const sockets = io.sockets.adapter.rooms.get(room);
     const uniqueNames = new Set();
     
@@ -36,6 +33,9 @@ function updateRoomStats(room) {
 }
 
 io.on('connection', (socket) => {
+    // This logs every time a device connects
+    console.log(`[CONNECT] Socket ID: ${socket.id}`);
+
     // --- Room Entry ---
     socket.on('join', (data) => {
         const { room, user } = data;
@@ -44,33 +44,57 @@ io.on('connection', (socket) => {
         socket.currentRoom = room;
         socket.userName = user;
 
+        // Log join event to Render
+        console.log(`[JOIN] User: ${user} | Room: ${room}`);
+
         io.in(room).emit('receive_message', { text: `${user} joined`, isSystem: true });
         updateRoomStats(room);
     });
 
     // --- Sync Logic ---
     socket.on('video_event', (data) => {
-        if (socket.currentRoom) socket.to(socket.currentRoom).emit('sync_video', data);
+        if (socket.currentRoom) {
+            socket.to(socket.currentRoom).emit('sync_video', data);
+        }
     });
 
+    // --- Episode / URL Change ---
     socket.on('change_url', (data) => {
         if (socket.currentRoom) {
-            socket.to(socket.currentRoom).emit('navigate_to', { url: data.url, user: data.user });
+            // This is the line that captures the episode link in your logs
+            console.log(`[EPISODE CHANGE] Room: ${socket.currentRoom} | User: ${data.user} | URL: ${data.url}`);
+            
+            socket.to(socket.currentRoom).emit('navigate_to', { 
+                url: data.url, 
+                user: data.user 
+            });
         }
     });
 
     // --- Chat System ---
     socket.on('send_message', (data) => {
-        if (socket.currentRoom) io.in(socket.currentRoom).emit('receive_message', data);
+        if (socket.currentRoom) {
+            // Log chat messages to Render
+            console.log(`[CHAT] ${socket.currentRoom} | ${data.user}: ${data.text}`);
+            io.in(socket.currentRoom).emit('receive_message', data);
+        }
     });
 
     // --- Disconnect Handler ---
     socket.on('disconnect', () => {
         if (socket.currentRoom) {
+            console.log(`[DISCONNECT] User: ${socket.userName} left Room: ${socket.currentRoom}`);
             updateRoomStats(socket.currentRoom);
         }
     });
 });
 
+// Health check to verify server is awake
+app.get('/', (req, res) => {
+    res.send('Server is live and logging.');
+});
+
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server live on ${PORT}`));
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
