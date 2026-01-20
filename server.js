adbf4186e726ad0d1a58c5c213f3d881de84e913
jafarovnihad <nihad.jafarov06@gmail.com>
2026-01-20 16:29:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,24 +6,36 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    },
+    cors: { origin: "*", methods: ["GET", "POST"] },
     pingTimeout: 60000,
     pingInterval: 25000
 });
 
-// --- Utility Functions ---
-function updateRoomCount(room) {
+// --- Room Data Storage ---
+const roomUsers = {}; 
+
+function updateRoomStats(room) {
     if (!room) return;
-    const roomSize = io.sockets.adapter.rooms.get(room)?.size || 0;
-    io.in(room).emit('room_count', { count: roomSize });
+    
+    // Get all unique usernames currently in the room
+    const sockets = io.sockets.adapter.rooms.get(room);
+    const uniqueNames = new Set();
+    
+    if (sockets) {
+        sockets.forEach(socketId => {
+            const s = io.sockets.sockets.get(socketId);
+            if (s && s.userName) uniqueNames.add(s.userName);
+        });
+    }
+
+    const userList = Array.from(uniqueNames);
+    io.in(room).emit('room_stats', { 
+        count: userList.length,
+        users: userList 
+    });
 }
 
 io.on('connection', (socket) => {
-    console.log(`[CONNECT] ${socket.id}`);
-
     // --- Room Entry ---
     socket.on('join', (data) => {
         const { room, user } = data;
@@ -32,58 +44,33 @@ io.on('connection', (socket) => {
         socket.currentRoom = room;
         socket.userName = user;
 
-        console.log(`[JOIN] ${user} -> ${room}`);
-
-        io.in(room).emit('receive_message', { 
-            text: `${user} joined the room`, 
-            isSystem: true 
-        });
-
-        updateRoomCount(room);
+        io.in(room).emit('receive_message', { text: `${user} joined`, isSystem: true });
+        updateRoomStats(room);
     });
 
-    // --- Video Sync Logic ---
+    // --- Sync Logic ---
     socket.on('video_event', (data) => {
-        if (socket.currentRoom) {
-            socket.to(socket.currentRoom).emit('sync_video', data);
-        }
+        if (socket.currentRoom) socket.to(socket.currentRoom).emit('sync_video', data);
     });
 
-    // --- Episode / URL Change ---
     socket.on('change_url', (data) => {
         if (socket.currentRoom) {
-            console.log(`https://www.change.org/ ${socket.currentRoom} | ${data.user} | ${data.url}`);
-            
-            socket.to(socket.currentRoom).emit('navigate_to', {
-                url: data.url,
-                user: data.user
-            });
+            socket.to(socket.currentRoom).emit('navigate_to', { url: data.url, user: data.user });
         }
     });
 
     // --- Chat System ---
     socket.on('send_message', (data) => {
-        if (socket.currentRoom) {
-            io.in(socket.currentRoom).emit('receive_message', data);
-        }
+        if (socket.currentRoom) io.in(socket.currentRoom).emit('receive_message', data);
     });
 
     // --- Disconnect Handler ---
     socket.on('disconnect', () => {
-        if (socket.currentRoom && socket.userName) {
-            console.log(`[LEAVE] ${socket.userName}`);
-            
-            io.in(socket.currentRoom).emit('receive_message', { 
-                text: `${socket.userName} disconnected`, 
-                isSystem: true 
-            });
-
-            updateRoomCount(socket.currentRoom);
+        if (socket.currentRoom) {
+            updateRoomStats(socket.currentRoom);
         }
     });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server live on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server live on ${PORT}`));
